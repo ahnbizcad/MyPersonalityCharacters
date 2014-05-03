07dc6af78357b9b372bbb1e0e8df4c7a3cbda8b9
ahnbizcad <ahnbizcad@gmail.com>
2014-05-03 14:10:46 -0700

    adjust navbar css

--- app/assets/javascripts/navbar.js.coffee
+++ app/assets/javascripts/navbar.js.coffee
@@ -3,5 +3,6 @@ ready = ->
 		offset: { top: $('.affixable').offset().top }
 	});
 	$('nav').height($('.affixable').height());
+	
 $(window).load(ready);
 $(window).on('page:load', ready);
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -252,5 +252,4 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  #config.assets.initialize_on_precompile = false
 end
