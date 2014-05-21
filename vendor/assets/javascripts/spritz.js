094eb6ef789121fd57a77289742afd1154fc7a69
ahnbizcad <ahnbizcad@gmail.com>
2014-05-21 02:57:22 -0700

    spritz test 7

--- vendor/assets/javascripts/spritz.js
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-  var SpritzSettings = {
-    clientId: "7025145b79bcd478b",
-    redirectUri:"http://mypersonalitycharacters.com/pages/login_success.html"
-  };
-</script>
