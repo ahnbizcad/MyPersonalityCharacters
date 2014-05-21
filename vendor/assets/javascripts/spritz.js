08423293d22a105a54cbdf5fa51d53fabe94ac54
ahnbizcad <ahnbizcad@gmail.com>
2014-05-21 02:48:54 -0700

    spritz test 5

--- vendor/assets/javascripts/spritz.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript" src="//sdk.spritzinc.com/jQuery/jquery-2.1.0.min.js"></script>
-<script type="text/javascript" src="//sdk.spritzinc.com/js/1.0/js/spritz.min.js"></script>
-<script type="text/javascript">
-	var SpritzSettings = {
-	  clientId: "7025145b79bcd478b",
-	  redirectUri:"http://mypersonalitycharacters.com/pages/login_success.html"
-	};
-</script>
