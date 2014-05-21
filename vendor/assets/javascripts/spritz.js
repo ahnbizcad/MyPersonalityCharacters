c847267d24308f839f978b04f740cfc556acf806
ahnbizcad <ahnbizcad@gmail.com>
2014-05-21 02:42:00 -0700

    spritz test 4

--- vendor/assets/javascripts/spritz.js
+++ vendor/assets/javascripts/spritz.js
@@ -3,6 +3,6 @@
 <script type="text/javascript">
 	var SpritzSettings = {
 	  clientId: "7025145b79bcd478b",
-	  redirectUri:"http://mypersonalitycharacters.herokuapp.com/pages/login_success.html"
+	  redirectUri:"http://mypersonalitycharacters.com/pages/login_success.html"
 	};
 </script>
